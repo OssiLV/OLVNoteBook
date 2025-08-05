b8d9b7a380bdcbfcb2728d8c2c02a2de5a0f6d85
OssiLV <vohoanganhtruong@gmail.com>
2025-08-05 22:31:38 +0700

    docs: linux cli cheat sheet content vi lang

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,147 +1,163 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 import vercel from "@astrojs/vercel";
 import mermaid from "astro-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: vercel(),
-	integrations: [
-		mermaid({
-			theme: 'neutral',
-			autoTheme: true
-		}),
-		starlight({
-			title: 'OssiLV',
-			tableOfContents: {minHeadingLevel: 2, maxHeadingLevel: 4},
-			locales: {
-				root: {
-					label: 'Việt Nam',
-					lang: 'vi',
-				},
-				'en': {
-					label: 'English',
-					lang: 'en',
-				}
-			},
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/OssiLV' },
-				{ icon: 'external', label: 'Portfolio', href: 'https://ossilv.github.io' },
-			],
-			sidebar: [
-				{
-					label: 'Giới thiệu',
-					translations: {
-						'en': 'Introduction'
-					},
-					items: [
-						{
-							label: 'Giới thiệu',
-							translations: {
-								'en': 'Introduction'
-							},
-							slug: 'introduction/introduction'
-						},
-					],
-				},
-				{
-					label: 'Networking',
-					items: [
-						// {
-						// 	label: 'Lộ trình Mạng',
-						// 	translations: {
-						// 		'en': 'Roadmap Networking'
-						// 	},
-						// 	slug: 'networking/roadmap'
-						// },
-						{
-							label: 'Mô hình OSI & TCP/IP',
-							translations: {
-								'en': 'OSI & TCP/IP Models'
-							},
-							slug: 'networking/osi_model_tcp_ip'
-						},
-						{
-							label: 'Địa chỉ IPv4',
-							translations: {
-								'en': 'IPv4 Addressing'
-							},
-							slug: 'networking/ipv4_addressing'
-						},
-						{
-							label: 'IPv4 Header',
-							translations: {
-								'en': 'IPv4 Header'
-							},
-							slug: 'networking/ipv4_header'
-						},
-						{
-							label: 'DNS (Domain Name System)',
-							translations: {
-								'en': 'DNS (Domain Name System)'
-							},
-							slug: 'networking/dns'
-						},
-						{
-							label: 'HTTP',
-							translations: {
-								'en': 'HTTP'
-							},
-							slug: 'networking/http'
-						},
-						{
-							label: 'SSL/TLS',
-							translations: {
-								'en': 'SSL/TLS'
-							},
-							slug: 'networking/ssl_tls'
-						},
-					],
-				},
-				{
-					label: 'Linux',
-					items: []
-				},
-				{
-					label: 'Git & GitHub',
-					items: []
-				},
-				{
-					label: 'CI/CD',
-					items: []
-				},
-				{
-					label: 'Tools',
-					items: [
-						{
-							label: 'Nginx',
-							translations: {
-								'en': 'Nginx'
-							},
-							slug: 'tools/nginx'
-						},
-					]
-				},
-				{
-					label: 'Cloud - AWS',
-					items: []
-				},
-				{
-					label: 'Monitoring & Logging',
-					items: []
-				},
-				{
-					label: 'DevSecOps',
-					items: []
-				},
-			],
-			customCss: [
-				'./src/styles/font.css',
-				// Fontsource files for to regular and semi-bold font weights.
-				'@fontsource/ibm-plex-sans/400.css',
-				'@fontsource/ibm-plex-sans/600.css',
-			],
-		}),
-	],
+    adapter: vercel(),
+    integrations: [
+        mermaid({
+            theme: "neutral",
+            autoTheme: true,
+        }),
+        starlight({
+            title: "OssiLV",
+            tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4 },
+            locales: {
+                root: {
+                    label: "Việt Nam",
+                    lang: "vi",
+                },
+                en: {
+                    label: "English",
+                    lang: "en",
+                },
+            },
+            social: [
+                {
+                    icon: "github",
+                    label: "GitHub",
+                    href: "https://github.com/OssiLV",
+                },
+                {
+                    icon: "external",
+                    label: "Portfolio",
+                    href: "https://ossilv.github.io",
+                },
+            ],
+            sidebar: [
+                {
+                    label: "Giới thiệu",
+                    translations: {
+                        en: "Introduction",
+                    },
+                    items: [
+                        {
+                            label: "Giới thiệu",
+                            translations: {
+                                en: "Introduction",
+                            },
+                            slug: "introduction/introduction",
+                        },
+                    ],
+                },
+                {
+                    label: "Networking",
+                    items: [
+                        // {
+                        // 	label: 'Lộ trình Mạng',
+                        // 	translations: {
+                        // 		'en': 'Roadmap Networking'
+                        // 	},
+                        // 	slug: 'networking/roadmap'
+                        // },
+                        {
+                            label: "Mô hình OSI & TCP/IP",
+                            translations: {
+                                en: "OSI & TCP/IP Models",
+                            },
+                            slug: "networking/osi_model_tcp_ip",
+                        },
+                        {
+                            label: "Địa chỉ IPv4",
+                            translations: {
+                                en: "IPv4 Addressing",
+                            },
+                            slug: "networking/ipv4_addressing",
+                        },
+                        {
+                            label: "IPv4 Header",
+                            translations: {
+                                en: "IPv4 Header",
+                            },
+                            slug: "networking/ipv4_header",
+                        },
+                        {
+                            label: "DNS (Domain Name System)",
+                            translations: {
+                                en: "DNS (Domain Name System)",
+                            },
+                            slug: "networking/dns",
+                        },
+                        {
+                            label: "HTTP",
+                            translations: {
+                                en: "HTTP",
+                            },
+                            slug: "networking/http",
+                        },
+                        {
+                            label: "SSL/TLS",
+                            translations: {
+                                en: "SSL/TLS",
+                            },
+                            slug: "networking/ssl_tls",
+                        },
+                    ],
+                },
+                {
+                    label: "Linux",
+                    items: [
+                        {
+                            label: "CLI cheat sheet",
+                            translations: {
+                                en: "CLI cheat sheet",
+                            },
+                            slug: "linux/cli_cheat_sheet",
+                        },
+                    ],
+                },
+                {
+                    label: "Git & GitHub",
+                    items: [],
+                },
+                {
+                    label: "CI/CD",
+                    items: [],
+                },
+                {
+                    label: "Tools",
+                    items: [
+                        {
+                            label: "Nginx",
+                            translations: {
+                                en: "Nginx",
+                            },
+                            slug: "tools/nginx",
+                        },
+                    ],
+                },
+                {
+                    label: "Cloud - AWS",
+                    items: [],
+                },
+                {
+                    label: "Monitoring & Logging",
+                    items: [],
+                },
+                {
+                    label: "DevSecOps",
+                    items: [],
+                },
+            ],
+            customCss: [
+                "./src/styles/font.css",
+                // Fontsource files for to regular and semi-bold font weights.
+                "@fontsource/ibm-plex-sans/400.css",
+                "@fontsource/ibm-plex-sans/600.css",
+            ],
+        }),
+    ],
 });
